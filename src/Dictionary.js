caf55ec6325f357589db99e3f3a70d7d7dfcc53b
Viktoria Vasyutinskaya <solovyanova.v@gmail.com>
2022-05-02 09:49:46 +0200

    added dictionary component which includes meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -22,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
